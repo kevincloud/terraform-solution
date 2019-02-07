f1ca8c670b4729836c4ead4d92d0fda4e75b4058
Kevin Cochran <kcochran@hashicorp.com>
2019-02-07 15:52:15 -0500

    fix user_data bug

--- modules/nginx/scripts/user_data.sh
+++ modules/nginx/scripts/user_data.sh
@@ -7,8 +7,7 @@ apt install -y nginx
 mkdir -p /data/nginx/cache
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-cd /root
-wget https://raw.githubusercontent.com/kevincloud/terraform-solution/master/modules/nxginx/scripts/content_server
+wget https://raw.githubusercontent.com/kevincloud/terraform-solution/master/modules/nginx/scripts/content_server
 sed -i -e 's/{HOSTNAME}/${web_server_address}/g' ./content_server
 cp ./content_server /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/content_server /etc/nginx/sites-enabled/content_server
