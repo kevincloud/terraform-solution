86c1fd99cbe5a53c39b17b03b7f204e83f4b8dd7
Kevin Cochran <kcochran@hashicorp.com>
2019-02-11 09:49:18 -0500

    Updates to webpage

--- scripts/index.php
+++ scripts/index.php
@@ -19,20 +19,46 @@ ini_set("allow_url_fopen", 1);
         font-variant: normal;
         text-transform: none;
         background: #BAC6D6 none no-repeat scroll 0 0;
+        margin: 0;
+        padding: 0;
+      }
+      .header {
+        position: relative;
+        top: 0;
+        width: 100%;
+        height: 100px;
+        border-bottom: 1px black solid;
+        -webkit-box-shadow: 5px 5px 30px 5px rgba(0,0,0,0.3); 
+        box-shadow: 5px 5px 30px 5px rgba(0,0,0,0.3);
+        font-size: 40px;
+        font-weight:bold;
+        padding: 5px;
+        background-color: white;
+      }
+      .main {
+        position: relative;
+        top: 0;
+        width: 100%;
+        padding: 5px;
       }
     </style>
   </head>
   <body>
+  <div class="header">
+    Temperature in Atlanta, GA
+  </div>
+  <div>
 <?php
 flush();
 ob_flush();
-$json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?zip=30127,us&appid=57d97ee2fb35e167103306cbef9279b3');
+$json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?zip=30303,us&appid=57d97ee2fb35e167103306cbef9279b3');
 $obj = json_decode($json);
 $temp = (($obj->main->temp - 273.15) * (9/5) + 32);
 $t = intval($temp);
 $s = intval(($temp - $p) * 10);
 if ($s >= 5) $t++;
-echo "<br><br>The temperature is currently " . $t . "&#8457; (".$temp.")<br>";
+echo "The temperature is currently " . $t . "&#8457; (".$temp.")<br>";
 ?>
+  </div>
   </body>
 </html>
