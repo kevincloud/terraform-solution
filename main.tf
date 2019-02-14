5f0d3a6c6485de173cff67e174bd3d6a85ead5eb
Kevin Cochran <kcochran@hashicorp.com>
2019-02-14 00:06:09 -0500

    update module version

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 module "peered-vpcs" {
     source  = "app.terraform.io/kevinspace/peered-vpcs/aws"
-    version = "1.0.7"
+    version = "1.0.8"
 
     aws_access_key = "${var.aws_access_key}"
     aws_secret_key = "${var.aws_secret_key}"
