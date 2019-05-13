16e5f2bf24d0b16f617964e4e7cbb2f9765a6bda
kevincloud <40193276+kevincloud@users.noreply.github.com>
2019-05-13 01:02:10 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
-output "us-east-ip" {
-    value = "${module.nginx-cdn.us-east-ip} / ${module.nginx-cdn.us-east-private-ip}"
-}
+# output "us-east-ip" {
+#     value = "${module.nginx-cdn.us-east-ip} / ${module.nginx-cdn.us-east-private-ip}"
+# }
 
-output "us-east-host" {
-    value = "${module.nginx-cdn.us-east-host}"
-}
+# output "us-east-host" {
+#     value = "${module.nginx-cdn.us-east-host}"
+# }
 
-output "us-west-ip" {
-    value = "${module.nginx-cdn.us-west-ip} / ${module.nginx-cdn.us-west-private-ip}"
-}
+# output "us-west-ip" {
+#     value = "${module.nginx-cdn.us-west-ip} / ${module.nginx-cdn.us-west-private-ip}"
+# }
 
-output "us-west-host" {
-    value = "${module.nginx-cdn.us-west-host}"
-}
+# output "us-west-host" {
+#     value = "${module.nginx-cdn.us-west-host}"
+# }
 
-output "eu-west-ip" {
-    value = "${module.nginx-cdn.eu-west-ip} / ${module.nginx-cdn.eu-west-private-ip}"
-}
+# output "eu-west-ip" {
+#     value = "${module.nginx-cdn.eu-west-ip} / ${module.nginx-cdn.eu-west-private-ip}"
+# }
 
-output "eu-west-host" {
-    value = "${module.nginx-cdn.eu-west-host}"
-}
+# output "eu-west-host" {
+#     value = "${module.nginx-cdn.eu-west-host}"
+# }
 
-output "webserver-private-ip" {
-    value = "${aws_instance.webserver.0.private_ip}"
-}
+# output "webserver-private-ip" {
+#     value = "${aws_instance.webserver.0.private_ip}"
+# }
 
-output "dns-address" {
-    value = "http://${aws_route53_record.privatemodules.name}"
-}
+# output "dns-address" {
+#     value = "http://${aws_route53_record.privatemodules.name}"
+# }
